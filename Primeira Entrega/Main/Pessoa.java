823994efd0f8a60e886a1bda585c878a06634790
Arthur-Matos <git.arthurmatos@gmail.com>
2020-10-19 22:17:06 -0300

    Esqueleto classe Pessoa
    Co-authored-by: Gabriel Sabanai <gabrielsabanaitrindade@gmail.com>

---
+++ Primeira Entrega/Main/Pessoa.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class Pessoa {
+    //Atributos
+    private String nome;
+    private String email;
+    private float totalRendimento;
+
+
+    //Metodos
+    public String getNome() {}
+    public void setNome(String nome){}
+    public String getEmail(){}
+    public void setEmail(String email){}
+    public float getTotalRendimento(){}
+    public float setTotalRendimento(float total){}
+}
