c80ae711a0d268b941d526b367757b4576c797fe
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2020-10-19 21:54:25 -0300

    Criado a classe Republica

---
+++ Primeira Entrega/Main/Republica.java
@@ -0,0 +1,30 @@
+package Main;
+
+public class Republica {
+    //Atributos 
+    private Pessoa pessoas[];
+    private Despesa despesas[];    
+    
+    
+    //Metodos 
+    public getPessoa(){
+    
+    }
+    public setPessoa(Pessoa pessoa){
+    
+    }
+    public getDespesa(){
+    
+    }
+    public setDespesa(Despesa despesa){
+    
+    }
+    
+    public regraIgualitaria(Despesa despesa, Pessoa pessoa) {
+        
+    }
+    
+    public regraProporcional(Despesa despesa, Pessoa pessoa) {
+        
+    }
+}
